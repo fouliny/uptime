f989dabce3ea80e8c8833bba3140f1043aa4c3ad
Leung Hsu <fouliny@gmail.com>
2023-05-18 08:17:44 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+  // 显示标题
+  SiteName: "服务监控",
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    "m794380585-cf6d109580b9562681611fda",
+    "m794380583-29eeb4ddde22375766e13caa",
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: "Homepage",
+      url: "https://www.siwu.cc/",
+    },
+    {
+      text: "nav",
+      url: "https://www.quguge.com",
+    },
+    {
+      text: "Blog",
+      url: "https://blog.siwu.cc",
+    },
+  ],
+};
